eb08e0e4b049d53821a83d9ee43acbae88e27ef9
enxa <enxa@mail.com>
2022-06-13 11:34:41 +0200

    Add categories and modal component

--- app.vue
+++ app.vue
@@ -1,4 +1,32 @@
+<script setup>
+  const categorySelect = ref(null)
+  const categories = ref([])
+  const joke = ref({})
+
+  const fetchCategory = async (url) => {
+    const response = await fetch(url)
+    const result = await response.json()
+    categories.value = result
+  }
+
+  const fetchJoke = async (url) => {
+    const response = await fetch(url)
+    const result = await response.json()
+    joke.value = result
+  }
+
+  onMounted(() => fetchCategory(`https://api.chucknorris.io/jokes/categories`))
+  watchEffect(() => fetchJoke(`https://api.chucknorris.io/jokes/random?category=${categorySelect.value}`))
+
+  const selectCategory = (category) => categorySelect.value = category
+</script>
+
 <template>
-  <section class="app">
+  <section class="categories rim">
+      <div v-for="category in categories">
+        <div @click="selectCategory(category)">{{category}}</div>
+      </div>
   </section>
+
+  <Modal :joke="joke.value" />
 </template>
---
+++ components/Modal.vue
@@ -0,0 +1,20 @@
+<script setup>
+  const props = defineProps({
+    joke: String
+  })
+
+  const { joke } = toRefs(props)
+
+  // watchEffect(async () => {
+  //   const { data: joke, pending, error, refresh } = await useFetch(`https://api.chucknorris.io/jokes/random?category=${category.value}`)
+
+  //   console.log(joke)
+  // })
+
+</script>
+
+<template>
+  <section class="modal rim">
+    {{joke}}
+  </section>
+</template>
